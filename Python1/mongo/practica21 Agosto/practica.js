9ff6848223dd241b16d1caea0246c65072b6f08e
23dereek <168207899+DerekNoguera@users.noreply.github.com>
2024-08-21 17:28:24 -0600

    x

---
+++ Python1/mongo/practica21 Agosto/practica.js
@@ -0,0 +1,111 @@
+db = connect('mongodb://localhost/libros')
+use.libros
+db.libros.insertMany([
+
+    { "titulo": "El Gran Gatsby", "autor": "F. Scott Fitzgerald", "genero": "Ficción", "año": 1925 },
+    
+    { "titulo": "Matar a un Ruiseñor", "autor": "Harper Lee", "genero": "Ficción", "año": 1960 },
+    
+    { "titulo": "1984", "autor": "George Orwell", "genero": "Ciencia Ficción", "año": 1949 },
+    
+    { "titulo": "Orgullo y Prejuicio", "autor": "Jane Austen", "genero": "Romance", "año": 1813 },
+    
+    { "titulo": "El Guardián entre el Centeno", "autor": "J.D. Salinger", "genero": "Ficción", "año": 1951 }
+    
+])
+db.libros.find().forEach(printjson)
+
+// Cambie / Actualice el género del libro llamado “1984” a que sea “Drama”
+db.libros.updateOne({titulo: "1984"},
+    {
+        $set: {genero: "Drama"}
+    }
+)
+// Elimine el documento de la colección "libros" donde el nombre del autor sea “J.D. Salinger”
+db.libros.deleteOne({autor: "J.D. Salinger"})
+
+                                        // Práctica - Filtrado
+
+// Liste el titulo de los libros que fueron publicados luego de 1940
+db.libros.find({año:{$gt: 1940}}).forEach(printjson)
+
+// Busque los libros que pertenezcan a un género específico. Elija un género y muestre los
+// títulos y autores de los libros en ese género.
+db.libros.find({genero: "Ficción"}).forEach(printjson)
+
+// Encuentre todos los libros escritos por un autor específico. Elija un autor y muestre los
+// títulos y géneros de los libros escritos por ese autor.
+db.libros.find({autor: "Jane Austen"}).forEach(printjson)
+
+// Liste únicamente los 2 primeros libros que fueron publicados luego de 1900 y ordenelos por
+// titulo de manera descendente
+db.libros.find({año:{$gt: 1900}}).limit(2).forEach(printjson)
+
+// Busque todos los libros. Una vez realizado esto y revisando cuáles son los valores que se
+// retornaron, retorne únicamente los 2 últimos libros del resultado anterior
+db.libros.find().sort({año: -1}).limit(2).forEach(printjson)
+
+                                        // Práctica - Agregación
+
+// Calcule el número total de libros en la colección "libros".
+db.libros.count()
+
+// Agrupe los libros en la colección "libros" por año y muestra la cantidad de libros publicados
+// cada año.
+db.libros.aggregate(
+    {
+    $group: {_id: "$año", count: {$sum: 1}}
+})
+
+// Calcule el año promedio de publicación para los libros.
+db.libros.aggregate([
+  {
+    $group: {
+      _id: null,
+      promedioAnio: { $avg: "$año" }
+    }
+  }
+]).forEach(printjson);
+
+// Obtenga la lista de géneros únicos presentes en la colección "libros" y muestra los géneros
+// en orden alfabético.
+db.libros.distinct("titulo").sort().forEach(printjson);
+
+
+
+
+
+                            // Práctica - Agregación
+
+// Describa lo que hace el siguiente código
+db.libros.aggregate([
+    {
+        $group:{
+            _id: "$genero",
+            totalDeLibros: {$sum: 1},
+            anioPromedioDePublicacion:{$avg: "$año"}
+        }
+    },
+    {$sort: {totalSales: -1}},
+    {$limit: 5}
+])
+
+                            // Práctica - Agregación
+
+// Describa lo que hace el siguiente código
+db.sales.aggregate([
+    {
+        $match:{
+            date: {$gte: ISODate("2022-01-01"), $lte: ISODate("2022-12-31")}
+        }
+    },
+    {
+        $group:{
+            _id: "$product",
+            totalSales:{ $sum: "$quantity"},
+            averagePrice:{$avg: "$price"}
+        }
+    },
+    {$sort:{totalSales: -1}},
+    {$limit: 5}
+])
--- Python1/mongo/scripts.js
+++ Python1/mongo/scripts.js
@@ -11,4 +11,62 @@ db.movies.updateOne({dummy_data: "Hola  "},
             dummy_data: "World"
         }
     })
+
+    db = connect('mongodb://localhost/movies');
+    db.moviesList.find().forEach(printjson);
+    db.moviesList.find({year: 1997}).forEach(printjson);
+    db.moviesList.find({year: {$eq: 1997}}).forEach(printjson);//equal
+    
+    list = db.moviesList.find({}, {title:1, _id:0});
+    console.log(list);
+    db.moviesList.find({year: 1997}, {title:1, _id:0}).forEach(printjson);
+    
+    db.moviesList.updateOne({title: "Casablanca"},
+      {
+        $set: {
+          year: 1942,
+          directtion: "Michael Curtiz"
+        }
+      }
+    )
+    
+    db.moviesList.updateMany({dummy_data: "hello"},
+      {
+        $set: {
+          dummy_data: "world"
+        }
+      }
+    )
+    
+    db.moviesList.updateOne({dummy_data: "world"},
+      {
+        $set: {
+          dummy_data: "hello"
+        }
+      }
+    )
+    
+    db.moviesList.deleteOne({title: "Casablanca"});
+    db.moviesList.deleteMany({title: "Titanic"});
+    
+    db.moviesList.find({genres: "Drama"}).forEach(printjson);
+    db.moviesList.find({genres: {$in:["Drama","Animation"]}}).forEach(printjson);
+    db.moviesList.find({genres: {$all:["Drama","Romance"]}}).forEach(printjson);
+    
+    db.moviesList.find({$or: [{year: 1997}, {year: 2001}]}).forEach(printjson);
+    db.moviesList.find({$and: [{genres: "Animation"}, {year: 2001}]}).forEach(printjson);
+    db.moviesList.find({$nor: [{genres: "Animation"}, {genres: "War"}]}).forEach(printjson);//not or
+    db.moviesList.find({genres: {$not: {$in: ["Animation", "War"]}}}).forEach(printjson);//lo mismo que el de abajo
+    db.moviesList.find({genres: {$nin: ["Animation", "War"]}}).forEach(printjson);//lo mismo que el de arriba pero con $nin
+    db.moviesList.find({year: {$ne: 2001}}).forEach(printjson);//not equal
+    db.moviesList.find({year: {$gt: 1997}}).forEach(printjson);//greater than
+    db.moviesList.find({year: {$gte: 1997}}).forEach(printjson);//greater than or equal
+    db.moviesList.find({year: {$lt: 1997}}).forEach(printjson);//less than
+    db.moviesList.find({year: {$lte: 1997}}).forEach(printjson);//less than or equal
+    db.moviesList.find({title: {$regex: "^S"}}).forEach(printjson);//starts with S
+    db.moviesList.find({title: {$not: {$regex: "^S"}}}).forEach(printjson);//Doesn't starts with S
+    db.moviesList.find().sort({year: 1}).forEach(printjson);//sort ascending
+    db.moviesList.find().sort({year: -1}).forEach(printjson);//sort descending
+    db.moviesList.find().limit(2).forEach(printjson);//limit results
+    db.moviesList.find().skip(2).forEach(printjson);//skip first 2 results
     
